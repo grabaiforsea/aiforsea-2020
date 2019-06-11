68f7a52cb945f43612b8d208a53b80596a72ba82
Marcus <marcuslimdw@gmail.com>
2019-06-11 08:52:39 +0800

    update code with callbacks

--- train.py
+++ train.py
@@ -1,5 +1,8 @@
 import os
 
+from functools import reduce
+from datetime import datetime
+
 import numpy as np
 import pandas as pd
 
@@ -11,14 +14,13 @@ from keras.models import Model
 from keras.layers import Dense
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.inception_v3 import InceptionV3
-
-
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, CSVLogger
 
 matlab_data = loadmat(os.path.join(os.curdir, 'devkit', 'cars_train_annos'))['annotations'][0, :]
 annotations = np.stack([np.concatenate([array.ravel() for array in row])[4:] for row in matlab_data])
 train_data = pd.DataFrame(annotations, columns=['target', 'filename'])
-train_data['target'] = train_data['target']
-train_data['filename'] = train_data['filename']
+
+n_classes = train_data['target'].nunique()
 
 batch_size = 32
 steps_per_epoch = ceil(len(train_data) * 0.8 / batch_size)
@@ -27,34 +29,45 @@ validation_steps = ceil(len(train_data) * 0.2 / batch_size)
 
 data_generator = ImageDataGenerator(validation_split=0.2)
 
-train_iterator = data_generator.flow_from_dataframe(train_data, 
-													directory='car_ims', 
-													x_col='filename', 
-													y_col='target', 
-													class_mode='sparse', 
-													batch_size=batch_size,
-													seed=0,
-													subset='training')
-																
-validation_iterator = data_generator.flow_from_dataframe(train_data, 
-														 directory='car_ims', 
-														 x_col='filename', 
-														 y_col='target', 
-														 class_mode='sparse', 
-														 batch_size=batch_size,
-														 seed=0,
-														 subset='validation')
+flow_kwargs = {'directory' : 'car_ims',
+               'x_col'     : 'filename',
+               'y_col'     : 'target',
+               'class_mode': 'sparse',
+               'batch_size': batch_size,
+               'seed'      : 0}
+
+train_iterator = data_generator.flow_from_dataframe(train_data,
+                                                    **flow_kwargs,
+                                                    subset='training')
+
+validation_iterator = data_generator.flow_from_dataframe(train_data,
+                                                         **flow_kwargs,
+                                                         subset='validation')
+
+callbacks = [ModelCheckpoint('aiforsea-model-{epoch}-{val_loss:.4f}.h5',
+                             monitor='val_loss',
+                             save_best_only=True,
+                             save_weights_only=True,
+                             mode='min'),
+             ReduceLROnPlateau(factor=0.2,
+                               patience=3),
+             CSVLogger(f"aiforsea-model-{datetime.now().strftime('%Y%m%d-%H%m%S')}.csv")]
+
+top_layers = [Dense(512, activation='relu'),
+              Dense(n_classes, activation='softmax')]
 
 inception_v3 = InceptionV3(include_top=False, pooling='max')
-model = Model(inputs=[inception_v3.input], outputs=[Dense(196, activation='softmax')(inception_v3.output)])
+top_combined = reduce(lambda first, second: second(first), top_layers, inception_v3.input)
+
+model = Model(inputs=[inception_v3.input],
+              outputs=[top_combined.output])
+
 model.compile('adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit_generator(train_iterator, 
-					steps_per_epoch=steps_per_epoch, 
-					epochs=epochs,
-					verbose=1, 
-					validation_data=validation_iterator,
-					validation_steps=validation_steps,
-					max_queue_size=10)
-					
-model.save_weights('model.h5')
+model.fit_generator(train_iterator,
+                    steps_per_epoch=steps_per_epoch,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=validation_iterator,
+                    validation_steps=validation_steps,
+                    max_queue_size=10)
